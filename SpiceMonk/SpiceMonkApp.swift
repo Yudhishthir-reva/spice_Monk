e81ddcd02fa32ff5c7481f078313bffdef7a92cb
Yudhishthir <yudhishthir@revateam.com>
2026-08-14 15:38:40 +0530

    Initial Commit

---
+++ SpiceMonk/SpiceMonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceMonkApp.swift
+//  SpiceMonk
+//
+//  Created by Devkinandan Dendwal on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SpiceMonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
